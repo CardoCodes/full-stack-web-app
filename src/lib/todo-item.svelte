f650a0fffb6bdab9a9990dd899f531418af55dd2
Ricardo Mendez <rmendez13@miners.utep.edu>
2021-12-17 02:16:28 +0000

    basic html components for app

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,16 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value="" />
+        <button aria-label="Mark done or Not done">Done/NotDone</button>
+    </form>
+
+    <form action="" methods="">
+        <input type="text "/>
+        <button aria-label="Save Todo">Save</button>
+    </form>
+
+    <form action="" methods="">
+        <button aria-label="Delte Todo">Delete</button>
+    </form>
+
+</div>
